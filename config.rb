a80a820cc072789f34dad9d557876873d1a4d21b
fightwithcrayon <contact@andrewthomashill.co.uk>
2017-01-11 15:33:06 +0000

    Reverting test – success
    Live remote server now set up

--- config.rb
+++ config.rb
@@ -23,6 +23,7 @@ css_dir = "stylesheets"
 sass_dir = "/wp-content/themes/clean-portfolio/sass"
 images_dir = "uploads"
 javascripts_dir = "/wp-content/themes/clean-portfolio/js"
+output_style = :compressed
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
