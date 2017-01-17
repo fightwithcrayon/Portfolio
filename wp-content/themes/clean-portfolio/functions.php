c727c39b24b2b7d274c4ee4a7cd0f18e8651b761
fightwithcrayon <contact@andrewthomashill.co.uk>
2017-01-17 12:52:16 +0000

    Remove W3 cache footer

--- wp-content/themes/clean-portfolio/functions.php
+++ wp-content/themes/clean-portfolio/functions.php
@@ -8,4 +8,6 @@ add_filter( 'pre_option_upload_path', function( $upload_path ) {
 add_filter( 'pre_option_upload_url_path', function( $upload_url_path ) {
     return 'http://www.andrewthomashill.co.uk/uploads';
 });
+
+add_filter( 'w3tc_can_print_comment', '__return_false', 10, 1 );
 ?>
