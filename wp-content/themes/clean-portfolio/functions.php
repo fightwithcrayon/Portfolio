b4168092d5218226ca1e6fe942cc7eae6f428913
fightwithcrayon <contact@andrewthomashill.co.uk>
2017-01-17 12:48:43 +0000

    Fix uploads

--- wp-content/themes/clean-portfolio/functions.php
+++ wp-content/themes/clean-portfolio/functions.php
@@ -2,10 +2,10 @@
 
 add_theme_support( 'post-thumbnails' );
 add_filter( 'pre_option_upload_path', function( $upload_path ) {
-    return '/img';
+    return '/uploads';
 });
 
 add_filter( 'pre_option_upload_url_path', function( $upload_url_path ) {
-    return 'http://www.andrewthomashill.co.uk/img';
+    return 'http://www.andrewthomashill.co.uk/uploads';
 });
 ?>
