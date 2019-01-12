0fe37547fe98f5d10eca22c5568337f5dfb42f97
fightwithcrayon <contact@andrewthomashill.co.uk>
2019-01-12 23:04:18 +0000

    Stupidly registered service worker during dev on my phone. To clear...

--- src/main.js
+++ src/main.js
@@ -9,3 +9,9 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+navigator.serviceWorker.getRegistrations().then(function (registrations) {
+  for (let registration of registrations) {
+    registration.unregister()
+  }
+})
