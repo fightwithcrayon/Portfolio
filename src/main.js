11b3b372ecb86e3f28e87e152bd4cc6b4236e09d
fightwithcrayon <contact@andrewthomashill.co.uk>
2019-01-12 23:17:02 +0000

    ...and reverse once job is done.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import './registerServiceWorker'
 import router from './router'
 
 Vue.config.productionTip = false
@@ -9,9 +8,3 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
-
-navigator.serviceWorker.getRegistrations().then(function (registrations) {
-  for (let registration of registrations) {
-    registration.unregister()
-  }
-})
