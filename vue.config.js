ab3761fe5a21dd3056ef56fae98c2eab94257227
fightwithcrayon <contact@andrewthomashill.co.uk>
2019-01-13 21:00:58 +0000

    Minify images

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 // vue.config.js
+var ImageminPlugin = require('imagemin-webpack-plugin').default
+
 module.exports = {
+  chainWebpack: config => {
+    config
+      .plugin('Imagemin')
+      .use(ImageminPlugin, [{
+        disable: process.env.NODE_ENV !== 'production', // Disable during development
+        pngquant: {
+          quality: '85-90'
+        }
+      }])
+  },
   css: {
     loaderOptions: {
       sass: {
