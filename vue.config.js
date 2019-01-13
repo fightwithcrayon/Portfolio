6c050c18fa3ec10e155bbf31afb233c1cbb4c0a9
fightwithcrayon <contact@andrewthomashill.co.uk>
2019-01-13 20:52:42 +0000

    Remove prerenderer

--- vue.config.js
+++ vue.config.js
@@ -1,30 +1,9 @@
 // vue.config.js
-const projects = require('./src/data/projects.js')
-const path = require('path')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
-const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
-
-let routes = projects.map(p => p.slug ? `/project/${p.slug}` : false).filter(r => r)
-routes.push('/')
-
 module.exports = {
-  chainWebpack: config => {
-    config
-      .plugin('PrerenderSPA')
-      .use(PrerenderSPAPlugin, [{
-        staticDir: path.join(__dirname, 'dist'),
-        routes: routes,
-        renderer: new Renderer({
-          maxConcurrentRoutes: 1,
-          renderAfterTime: 5000,
-          headless: false
-        })
-      }])
-  },
   css: {
     loaderOptions: {
       sass: {
-        data: `@import "@/scss/variables.scss";@import "@/scss/mixins.scss";`
+        data: `@import "@/scss/variables.scss"; @import "@/scss/mixins.scss";`
       }
     }
   }
