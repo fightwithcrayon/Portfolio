d5689591f482fca653f8dde09acc4c290d246a6e
Andrew <>
2019-01-17 13:32:25 +0000

    Better Firebase logging function

--- functions/index.js
+++ functions/index.js
@@ -6,18 +6,27 @@ admin.initializeApp();
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.ping = functions.https.onRequest((request, response) => {
-  let ip = request.ip || false
+  let ip = request.ip.toString()
   let filteredIps = [
     '92.232.156.202', // Home
+    '51.52.173.110', // Work
     '204.187.14.73', '204.187.14.74', '204.187.14.75', '204.187.14.76', '204.187.14.77' // GTMetrix
   ]
-  if (!ip || !filteredIps.includes(ip)) {
-    let date = new Date()
-    admin.database().ref(`/visits/${ip.replace(/\./g, '_')}`).push({
+  let date = new Date()
+  let updates = {}
+
+  if (!filteredIps.includes(ip)) {
+    updates[`/log/${date}`] = {
       ip: ip,
       time: date.toLocaleString(),
       page: request.query.page
-    })
+    }
+    updates[`/visits/${ip.replace(/\./g, '_')}`] = {
+      ip: ip,
+      time: date.toLocaleString(),
+      page: request.query.page
+    }
   }
+  admin.database().ref().update(updates);
   return response.status(200).end();
 });
